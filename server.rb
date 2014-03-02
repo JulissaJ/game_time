1387492a5d4de5ebe9491ce43931ca4b8d83af9b
JulissaJ <julissa.jansen@gmail.com>
2014-03-02 15:29:14 -0500

    removed commented lines

--- server.rb
+++ server.rb
@@ -3,47 +3,6 @@ require 'pry'
 require 'shotgun'
 
 
-
-# @scores = [
-#   {
-#     home_team: "Patriots",
-#     away_team: "Broncos",
-#     home_score: 7,
-#     away_score: 3
-#   },
-#   {
-#     home_team: "Broncos",
-#     away_team: "Colts",
-#     home_score: 3,
-#     away_score: 0
-#   },
-#   {
-#     home_team: "Patriots",
-#     away_team: "Colts",
-#     home_score: 11,
-#     away_score: 7
-#   },
-#   {
-#     home_team: "Steelers",
-#     away_team: "Patriots",
-#     home_score: 7,
-#     away_score: 21
-#   }
-# ]
-
-# @leaderboard = Hash.new{|h,k| h[k]=Hash.new(0)}
-# @scores.each do |game|
-#   if game[:home_score].to_i > game[:away_score].to_i
-#     @leaderboard[game[:home_team]][:w] += 1
-#     @leaderboard[game[:away_team]][:l] += 1
-#   else
-#     @leaderboard[game[:away_team]][:w] += 1
-#     @leaderboard[game[:home_team]][:l] += 1
-#   end
-# end
-
-
-
 get '/' do
   @board = 'Leader Board'
   @league = 'NFL'
